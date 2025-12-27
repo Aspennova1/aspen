6c191260f8fa032f5d5c16735c04ee033b2bd514
Aspennova1 <info@aspen8.onmicrosoft.com>
2025-12-27 16:36:45 +0530

    config changes

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  reactStrictMode: true,
-  distDir: 'build',
-  output: 'standalone'
+  // output: 'standalone'
   /* config options here */
 };
 
